3cc01c38f081469a03184f0b746dbb452e2df292
undead-gio <gio.com@hotmail.it>
2018-06-14 00:10:19 +0200

    feature

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 let latitude = ''
     longitude = ''
-
-
-
+    id_entity = ''
+    type_entity = ''
 
 function success(position){
     latitude = position.coords.latitude
@@ -23,11 +22,40 @@ function success(position){
 
 function error(err){
     console.error(err)
+    alert("Per l'utilizzo di questa applicazione è necessaria la geolocalizzazione");
+}
+
+function GeneralError() {
+  $('.error').append('<h2 class="error-message"> La ricerca non ha prodotto nessun risultato, prova a ricaricare la pagina :) </h2>');
 }
 
 
-function datiArrivati(risposta){
-    console.log(risposta)
+function datiArrivati(city){
+    console.log(city);
+    id_entity = city.location.entity_id;
+    type_entity = city.location.entity_type;
+    $('#position').append(city.location.city_name + ', ' + city.location.title);
+
+    $.ajax({
+            url: 'https://developers.zomato.com/api/v2.1/search',
+            dataType: 'json',
+            data: {
+                entity_id : id_entity,
+                entity_type: type_entity,
+                count: 10,
+                lat: latitude,
+                lon: longitude,
+                radius: 3,
+                sort: 'real_distance',
+                order: 'asc'
+            },
+            headers: {
+                "user-key": "f7b021777643a1e7a87e4d0f3a2792ff"
+            },
+            success: Nearby,
+            error: GeneralError
+        })
+
     $.ajax({
             url: 'https://developers.zomato.com/api/v2.1/collections',
             dataType: 'json',
@@ -39,7 +67,8 @@ function datiArrivati(risposta){
             headers: {
                 "user-key": "f7b021777643a1e7a87e4d0f3a2792ff"
             },
-            success: Collection
+            success: Collection,
+            error: GeneralError
         })
 }
 
@@ -47,15 +76,23 @@ function datiArrivati(risposta){
 function Collection(collezioni){
     console.log(collezioni)
     collezioni.collections.forEach(function(item, i){
-       $('#card').append(
+       $('#card-coll').append(
             '<h2>' + item.collection.title + '</h2>' +
-            '<img class="coll_name" src="' + item.collection.image_url + '"/>'
+            '<img class="coll_img" src="' + item.collection.image_url + '"/>'
        )
     })
 }
 
 
-
+function Nearby(near) {
+  console.log(near)
+  near.restaurants.forEach(function(item, i){
+     $('#card-near').append(
+          '<img class="near-img" src="' + item.restaurant.photos_url + '"/>' +
+          '<h1 class="restaurant-card-title">' + item.restaurant.name + '</h1>'
+     )
+  })
+}
 
 
 
