de99650b78dd40e052a3118220cbff9e01dc0e05
undead-gio <gio.com@hotmail.it>
2018-06-13 17:53:10 +0200

    ajax call implement

---
+++ js/index.js
@@ -0,0 +1,69 @@
+let latitude = ''
+    longitude = ''
+
+
+
+
+function success(position){
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    $.ajax({
+        url: 'https://developers.zomato.com/api/v2.1/geocode',
+        dataType: 'json',
+        data: {
+            lat: latitude,
+            lon: longitude,
+        },
+        headers: {
+            "user-key": "f7b021777643a1e7a87e4d0f3a2792ff"
+        },
+        success: datiArrivati
+    })
+}
+
+function error(err){
+    console.error(err)
+}
+
+
+function datiArrivati(risposta){
+    console.log(risposta)
+    $.ajax({
+            url: 'https://developers.zomato.com/api/v2.1/collections',
+            dataType: 'json',
+            data: {
+                lat: latitude,
+                lon: longitude,
+                count: 10,
+            },
+            headers: {
+                "user-key": "f7b021777643a1e7a87e4d0f3a2792ff"
+            },
+            success: Collection
+        })
+}
+
+
+function Collection(collezioni){
+    console.log(collezioni)
+    collezioni.collections.forEach(function(item, i){
+       $('#card').append(
+            '<h2>' + item.collection.title + '</h2>' +
+            '<img src="' + item.collection.image_url + '"/>'
+       )
+    })
+}
+
+
+
+
+
+
+
+
+
+if(navigator.geolocation){
+    console.log('location service is active')
+    navigator.geolocation.getCurrentPosition(success, error)
+
+}
