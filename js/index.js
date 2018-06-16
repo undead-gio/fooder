4bddf3db7956db305cbbf3cc9a78bbbd0e5e5044
undead-gio <gio.com@hotmail.it>
2018-06-16 13:17:24 +0200

    work on graphic

--- js/index.js
+++ js/index.js
@@ -39,6 +39,7 @@ function datiArrivati(city){
     id_entity = city.location.entity_id;
     type_entity = city.location.entity_type;
     $('#position').append(city.location.city_name + ', ' + city.location.title);
+    $('.nav-loc').append(city.location.city_name + ', ' + city.location.title);
 
     $.ajax({
             url: 'https://developers.zomato.com/api/v2.1/search',
@@ -49,9 +50,9 @@ function datiArrivati(city){
                 count: 10,
                 lat: latitude,
                 lon: longitude,
-                radius: 3,
+                radius: 5,
                 sort: 'real_distance',
-                order: 'asc'
+                order: 'desc'
             },
             headers: {
                 "user-key": "f7b021777643a1e7a87e4d0f3a2792ff"
@@ -80,11 +81,35 @@ function Nearby(near) {
   console.log(near)
   near.restaurants.forEach(function(item, i){
      $('.near').append(
-       '<div class="row"><div class="col-sm-12"><div class="card-near" data-id="' + item.restaurant.id +
-       '"><img class="near-img" src="'
-       + item.restaurant.featured_image + '"/>' +
-       '<span class="restaurant-card-title">' + item.restaurant.name +
-       '</span></div></div></div></div>'
+       '<div class="row"><div class="col-md-2"></div><div class="col-sm-12 col-md-8"><div class="card-near" data-id="' + item.restaurant.id + '">' +
+       '<div class="row" style="height:100%">' +
+         '<div class="col-sm-5">' +
+           '<img class="near-img" src="'
+           + item.restaurant.featured_image + '"/>' +
+         '</div>' +
+         '<div class="col-sm-5 align-self-center">' +
+           '<h3 class="restaurant-card-title no-marg w-400">' + item.restaurant.cuisines +
+           '</h3>'+
+           '<h2 class="restaurant-card-title rest-name w-700">' + item.restaurant.name +
+           '</h2>'+
+           '<h4 class="restaurant-card-address no-marg w-300">' + item.restaurant.location.address +
+           '</h4>'+
+           '<h2 class="restaurant-card-title w-400" style="margin-top:16%">' + item.restaurant.currency +
+           '</h2>'+
+         '</div>' +
+         '<div class="col-sm-1">' +
+         '</div>' +
+         '<div class="col-sm-2 rating">' +
+          '<div class="cont-rec" style="background-color: #'+ item.restaurant.user_rating.rating_color +'">' +
+             '<h2 class="restaurant-card-title w-700">' + item.restaurant.user_rating.aggregate_rating +
+             '</h2>'+
+          '</div>' +
+          '<h3 class="restaurant-card-title w-300" style="text-align:center">' + item.restaurant.user_rating.votes+
+          ' Voti</h3>'+
+         '</div>' +
+       '</div>' +
+       '</div><div class="col-md-2"></div>' +
+       '</div></div></div>'
      )
   })
   $('.card-near').click(function(event) {
@@ -116,9 +141,7 @@ function Collection(collezioni){
 }
 
 
-
 if(navigator.geolocation){
     console.log('location service is active')
     navigator.geolocation.getCurrentPosition(success, error)
-
 }
